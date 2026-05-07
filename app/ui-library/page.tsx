5fa72fd26c479ed88f7114f96364d340bd1abc1e
jasarora07 <jaspreet07arora@gmail.com>
2026-05-08 04:28:38 +0530

    Delete app directory

--- app/ui-library/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentShowcase } from "@/components/ui-library/showcase"
-
-export default function UILibraryPage() {
-  return (
-    <main className="py-12">
-      <ComponentShowcase />
-    </main>
-  )
-}
