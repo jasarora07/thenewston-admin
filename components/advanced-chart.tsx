59a283c17fac714472613aaa050fc4b44c0339c7
jasarora07 <jaspreet07arora@gmail.com>
2026-05-08 16:18:53 +0530

    Update advanced-chart.tsx

--- components/advanced-chart.tsx
+++ components/advanced-chart.tsx
@@ -5,32 +5,27 @@ export default function AdvancedChart({ symbol }: { symbol: string }) {
   const container = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (container.current) {
-      container.current.innerHTML = ""
-      const script = document.createElement("script")
-      script.src = "https://s3.tradingview.com/tv.js"
-      script.type = "text/javascript"
-      script.onload = () => {
-        if (window.TradingView) {
-          new window.TradingView.widget({
-            "autosize": true,
-            "symbol": symbol,
-            "interval": "D",
-            "timezone": "Etc/UTC",
-            "theme": "dark",
-            "style": "1",
-            "locale": "en",
-            "toolbar_bg": "#f1f3f6",
-            "enable_publishing": false,
-            "hide_top_toolbar": false,
-            "allow_symbol_change": true,
-            "container_id": "tv_chart_container"
-          })
-        }
+    const script = document.createElement("script")
+    script.src = "https://s3.tradingview.com/tv.js"
+    script.async = true
+    script.onload = () => {
+      if (container.current && window.TradingView) {
+        new window.TradingView.widget({
+          "autosize": true,
+          "symbol": symbol,
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "enable_publishing": false,
+          "hide_top_toolbar": false,
+          "container_id": container.current.id
+        })
       }
-      container.current.appendChild(script)
     }
-  }, [symbol]) // Re-renders when symbol changes
+    document.head.appendChild(script)
+  }, [symbol])
 
-  return <div id="tv_chart_container" ref={container} className="h-full w-full" />
+  return <div id={`tv_chart_${symbol}`} ref={container} className="h-full w-full" />
 }
