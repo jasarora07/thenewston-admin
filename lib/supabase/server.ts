0cd5fbb7a77fe73acbb794d2c14fa90f65d91f2c
jasarora07 <jaspreet07arora@gmail.com>
2026-05-09 03:39:50 +0530

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,7 +6,8 @@ export async function createClient() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    // CHANGE THIS LINE BELOW:
+    process.env.SUPABASE_SERVICE_ROLE_KEY!, 
     {
       cookies: {
         getAll() {
@@ -18,9 +19,7 @@ export async function createClient() {
               cookieStore.set(name, value, options)
             )
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            // This is expected when called from Server Components
           }
         },
       },
