78863ba76244754c0cd2ab404cc3cacb7b84e3ec
jasarora07 <jaspreet07arora@gmail.com>
2026-05-10 17:48:36 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,12 @@
 import { MetadataRoute } from 'next'
-
+ 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: [
-          '/api/',     // Don't crawl your internal API routes
-          '/admin/',   // Don't crawl admin panels
-          '/_next/',   // Don't crawl Next.js internal files
-        ],
+        disallow: '/admin/', 
       },
     ],
     sitemap: 'https://thenewston.com/sitemap.xml',
