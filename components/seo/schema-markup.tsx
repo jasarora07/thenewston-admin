98fd09078639dcdb7cc2c2b98c1a1b539cc9c7f6
jasarora07 <jaspreet07arora@gmail.com>
2026-05-16 23:05:46 +0530

    Create schema-markup.tsx

---
+++ components/seo/schema-markup.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+interface SchemaProps {
+  name: string
+  description: string
+  url: string
+}
+
+export default function CalculatorSchema({ name, description, url }: SchemaProps) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebApplication",
+    "@id": url,
+    "url": url,
+    "name": name,
+    "description": description,
+    "applicationCategory": "BusinessApplication",
+    "operatingSystem": "All",
+    "browserRequirements": "Requires JavaScript. Requires HTML5.",
+    "offers": {
+      "@type": "Offer",
+      "price": "0.00",
+      "priceCurrency": "USD"
+    },
+    "author": {
+      "@type": "Organization",
+      "name": "The Newston Terminal",
+      "url": "https://thenewston.com"
+    }
+  }
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  )
+}
